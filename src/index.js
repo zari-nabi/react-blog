589679ca4d80c329762235a2c8389fb68886d477
unknown <Nabi@nabi-it.esfahanmet.net>
2019-01-23 12:16:05 +0330

    add navbar - delete app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route ,Link } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => {
+    return <h1>this is Home</h1>
+}
+
+const About = () => {
+    return <h1>this is About</h1>
+}
+
+ReactDOM.render(
+    <BrowserRouter>
+    <div>
+        <Navbar/>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Footer/>
+    </div>
+    </BrowserRouter>
+
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
