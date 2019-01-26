0b1fe9d7acf744e50ab6682b490b0b4403f6f6a3
unknown <Nabi@nabi-it.esfahanmet.net>
2019-01-26 14:25:05 +0330

    signUp & validation

---
+++ src/components/SignUp/index.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { validateAll } from "indicative";
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password_confirmation: '',
+            errors: {}
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const data = this.state;
+        const rules = {
+            name: 'required|string',
+            email: 'required|email',
+            password: 'required|string|min:6|confirmed'
+
+        }
+
+        const message={
+            required : 'the {{field}} is required',
+            'email.email' : 'the format is invalid',
+            'password.confirmed' : 'the password does not match'
+        }
+
+        validateAll(data, rules, message)
+            .then(() => { console.log('success') })
+            .catch((errors) => {
+                const formatedErrors = {};
+                errors.forEach(error => formatedErrors[error.field] = error.message);
+                this.setState({
+                    errors: formatedErrors
+                })
+            }
+            )
+
+    }
+    render() {
+        return (
+            <div className="mh-fullscreen bg-img center-vh p-20" style={{ backgroundImage: 'url(assets/img/bg-girl.jpg)' }}>
+                <div className="card card-shadowed p-50 w-400 mb-0" style={{ maxWidth: '100%' }}>
+                    <h5 className="text-uppercase text-center">Register</h5>
+                    <br />
+                    <br />
+                    <form className="form-type-material">
+                        <div className="form-group">
+                            <input name="name" onChange={this.handleChange} type="text" className="form-control" placeholder="Username" />
+                            {
+                                this.state.errors['name'] &&
+                                <small className="text-danger">{this.state.errors['name']}</small>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <input name="email" onChange={this.handleChange} type="text" className="form-control" placeholder="Email address" />
+                            {
+                                this.state.errors['email'] &&
+                                <small className="text-danger">{this.state.errors['email']}</small>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <input name="password" onChange={this.handleChange} type="password" className="form-control" placeholder="Password" />
+                            {
+                                this.state.errors['password'] &&
+                                <small className="text-danger">{this.state.errors['password']}</small>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <input name="password_confirmation" onChange={this.handleChange} type="password" className="form-control" placeholder="Password (confirm)" />
+                        </div>
+                        <br />
+                        <button onClick={this.handleSubmit} className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
+                    </form>
+                    <hr className="w-30" />
+                    <p className="text-center text-muted fs-13 mt-20">Already have an account?
+                <a href="login.html">Sign in</a>
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route ,Link } from "react-router-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
 import Footer from "./components/Footer";
 import CreateArticle from "./components/CreateArticle";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import SingleArticle from "./components/SingleArticle";
 
 import * as serviceWorker from './serviceWorker';
 
-
+const Main = withRouter(({ location }) => {
+    return (
+        <div>
+            {
+                location.pathname !== "/login" && location.pathname !== "/signup" &&
+                <Navbar />
+            }
+            <Route exact path="/" component={Welcome} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/article/:slug" component={SingleArticle} />
+            <Route path="/articles/create" component={CreateArticle} />
+            {
+                location.pathname !== "/login" && location.pathname !== "/signup" &&
+                <Footer />
+            }
+        </div>
+    )
+})
 
 ReactDOM.render(
     <BrowserRouter>
-    <div>
-        <Navbar/>
-        <Route exact path="/" component={Welcome} />
-        <Route path="/login" component={Login} />
-        <Route path="/article/:slug" component={SingleArticle} />
-        <Route path="/articles/create" component={CreateArticle} />
-        <Footer/>
-    </div>
+        <Main />
     </BrowserRouter>
 
     , document.getElementById('root'));
