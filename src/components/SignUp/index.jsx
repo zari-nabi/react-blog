bb3251d1a17d841951bb4a5ebfd3137ed670eee0
unknown <Nabi@nabi-it.esfahanmet.net>
2019-01-29 08:25:27 +0330

    authenticating

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -48,6 +48,7 @@ class SignUp extends React.Component {
                      password:this.state.password
                  }).then(response =>{
                      localStorage.setItem('user',JSON.stringify(response.data.data));
+                     this.props.setAuthUser(response.data.data);
                      this.props.history.push('/');
                  }).catch(errors => {
                      console.log(errors.response)
--- src/index.js
+++ src/index.js
@@ -12,29 +12,64 @@ import SingleArticle from "./components/SingleArticle";
 
 import * as serviceWorker from './serviceWorker';
 
-const Main = withRouter(({ location }) => {
-    return (
-        <div>
-            {
-                location.pathname !== "/login" && location.pathname !== "/signup" &&
-                <Navbar />
-            }
-            <Route exact path="/" component={Welcome} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/article/:slug" component={SingleArticle} />
-            <Route path="/articles/create" component={CreateArticle} />
-            {
-                location.pathname !== "/login" && location.pathname !== "/signup" &&
-                <Footer />
-            }
-        </div>
-    )
-})
+class App extends React.Component {
 
-ReactDOM.render(
+    constructor() {
+      super();
+  
+      this.state = {
+        authUser: null
+      };
+    }
+  
+    componentDidMount() {
+      const user = localStorage.getItem('user')
+  
+      if (user) {
+        this.setState({
+          authUser: JSON.parse(user)
+        })
+      } 
+    }
+
+    setAuthUser = (authUser) => {
+        this.setState({
+          authUser
+        })
+    }
+  
+    render() {
+      const { location } = this.props;
+      return (
+        <div>
+          {
+            location.pathname !== '/login' && location.pathname !== '/signup' &&
+            <Navbar authUser={this.state.authUser} />
+          }
+          <Route exact path="/" component={Welcome} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" render = {(props) => <SignUp {...props} setAuthUser = {this.setAuthUser}/>} />
+          <Route path="/article/:slug" component={SingleArticle} />
+          <Route path="/articles/create" component={CreateArticle} />
+          {
+            location.pathname !== '/login' && location.pathname !== '/signup' &&
+            <Footer />
+          }
+        </div>
+      );
+    }
+  }
+  
+  
+  const Main = withRouter((props) => {
+    return (
+      <App {...props} />
+    );
+  });
+  
+  ReactDOM.render(
     <BrowserRouter>
-        <Main />
+      <Main />
     </BrowserRouter>
 
     , document.getElementById('root'));
